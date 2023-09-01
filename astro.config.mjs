0002f7884e496fd9037fd1edfe9ff2725c84e59f
Mickael Sermont <sermont.mickael@hotmail.fr>
2023-09-01 07:16:00 +0200

    feat: install shadcn ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
