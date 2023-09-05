16722b2f5987eebae626395272c1a6ac72721f05
Mickael Sermont <sermont.mickael@hotmail.fr>
2023-09-05 14:30:56 +0200

    feat: update blog header

--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -1,5 +1,15 @@
 import { getCollection } from "astro:content";
 
+export async function getCategories() {
+  const posts = await getCollection("blog");
+
+  const categories = [
+    ...new Set([].concat.apply(posts.map((post) => post.data.category))),
+  ];
+
+  return categories;
+}
+
 export async function getPosts() {
   const posts = (await getCollection("blog")).sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
