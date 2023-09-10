b5b97708deb81f0f43f6ec4cc079acd0cc5ca1cb
Mickael Sermont <sermont.mickael@hotmail.fr>
2023-09-10 17:18:58 +0200

    feat: update site url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://astro-nomy.vercel.app",
   integrations: [
     mdx({
       syntaxHighlight: 'shiki',
