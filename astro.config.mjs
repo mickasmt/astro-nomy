d9353df97e34aeccf783bdedc1ed31575bac9814
Mickael Sermont <sermont.mickael@hotmail.fr>
2023-09-12 19:45:36 +0200

    ci: check output

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,8 +20,9 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel({
     analytics: true,
+    imageService: true,
   }),
 });
