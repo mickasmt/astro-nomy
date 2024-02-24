8266062039d6388650a9ef68745ad8a62a65ca42
mickasmt <sermont.mickael@hotmail.fr>
2024-02-24 23:48:02 +0100

    refactor: upgrade astro-icon to v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,20 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nomy.vercel.app",
   integrations: [
     mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'github-dark-dimmed' },
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "github-dark-dimmed" },
       gfm: true,
     }),
+    icon(),
     sitemap(),
     react(),
     tailwind({
