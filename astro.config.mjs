ffa1feb0d9cdb0c40f82301b8c953816fd40debb
mickasmt <sermont.mickael@hotmail.fr>
2024-05-07 11:05:01 +0200

    fix: change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import db from "@astrojs/db";
-import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import simpleStackForm from "simple-stack-form";
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,6 +26,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     db(),
+    simpleStackForm(),
   ],
   output: "hybrid",
   adapter: vercel({
